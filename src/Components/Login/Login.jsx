cbfda964e71084ca3dff6eb25f7df36bd7949bec
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-07-13 21:58:56 +0600

    added signUp and implement lottie

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -3,7 +3,9 @@ import React, { useContext } from 'react';
 import app from '../firebase/firebase.config';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../AuthProviders/AuthProviders';
-import google from '../../assets/logo/google.png'
+import google from '../../assets/logo/google.png';
+import Lottie from "lottie-react";
+import lottie from '../../assets/logo/animation2.json';
 
 const Login = () => {
 
@@ -14,21 +16,21 @@ const Login = () => {
 
     //sign in by google
     const googleProvider = new GoogleAuthProvider();
-    const handleLoginWithGoogle =()=>{
+    const handleLoginWithGoogle = () => {
         signInWithPopup(auth, googleProvider)
-        .then(result=>{
-            const user = result.user;
-            console.log(user)
-            navigate(from,{replace: true})
-            //toast.success('Successfully login by Google!')
-        })
-        .catch(error=>{
-            const errorMessage = error.message;
-        })
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+                navigate(from, { replace: true })
+                //toast.success('Successfully login by Google!')
+            })
+            .catch(error => {
+                const errorMessage = error.message;
+            })
     }
 
-    const {logIn} =useContext(AuthContext);
-    const handleLogin =(event)=>{
+    const { logIn } = useContext(AuthContext);
+    const handleLogin = (event) => {
         event.preventDefault();
 
         const form = event.target;
@@ -37,29 +39,33 @@ const Login = () => {
         console.log(email, password);
 
         logIn(email, password)
-        .then(result=>{
-            const loggedUser =result.user;
-            console.log(loggedUser)
-            navigate(from,{replace: true})
-            form.reset()
-           // toast.success('Successfully login!')
-           
-        })
-        .catch(error=>{
-            console.log(error.message)
-        })
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser)
+                navigate(from, { replace: true })
+                form.reset()
+                // toast.success('Successfully login!')
+
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
     }
 
 
 
     return (
         <div>
-            <div className="hero pt-10">
-                <div className="hero-content flex-col">
-                    <div className="text-center">
-                        <h1 className="text-4xl font-bold">Please Login now!</h1>
+            <h1 className="text-3xl text-center font-bold pt-8">Please Login now!</h1>
+            <div className="hero pt-2">
+                <div className="hero-content">
+
+                    <div className='mr-10'>
+                        <Lottie className='w-64 h-64' animationData={lottie} loop={true} />
                     </div>
-                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+
+
+                    <div className="card w-full  shadow-xl bg-base-100">
                         <form onSubmit={handleLogin} className="card-body">
                             <div className="form-control">
                                 <label className="label">
@@ -76,8 +82,8 @@ const Login = () => {
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                                 <label className="label">
-                                    <a href="#" className="label-text-alt ">Don't have account? <Link className='link link-hover text-red-600 ml-2' 
-                                    to='/signUp'>Registration</Link></a>
+                                    <a href="#" className="label-text-alt ">Don't have account? <Link className='link link-hover text-red-600 ml-2'
+                                        to='/signUp'>Registration</Link></a>
                                 </label>
                             </div>
                             <div className="form-control mt-4">
@@ -86,7 +92,7 @@ const Login = () => {
                         </form>
                         <div className='text-center'>
                             <p>Or</p>
-                            <div className=" mt-2">
+                            <div className=" mt-2 mb-2">
                                 <button onClick={handleLoginWithGoogle} className="btn btn-sm btn-active hover:bg-blue-600 hover:text-white btn-ghost">
                                     <img className='w-8 h-8 mr-2' src={google} /> Login by Google</button>
                             </div>
--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,9 +1,108 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { AuthContext } from '../AuthProviders/AuthProviders';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import Lottie from "lottie-react";
+import lottie from '../../assets/logo/amination.json'
 
 const SignUp = () => {
+
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+    const { createUser } = useContext(AuthContext);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/'
+
+    const handleRegister = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirm = form.confirm.value
+
+        console.log(name, email, password, confirm);
+        setError('');
+        setSuccess('');
+        if (password !== confirm) {
+            setError("Your password did't match");
+            return
+        }
+        else if (password.length < 6) {
+            setError("Password must be 6 characters or longer")
+            return
+        }
+        else {
+            setSuccess('Registration successful!');
+            navigate(from, { replace: true })
+        }
+
+        createUser(email, password)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser)
+
+            })
+            .catch(error => {
+                console.log(error)
+                setError(error.message)
+            })
+
+    }
     return (
         <div>
-            <h1>This is Sign Up</h1>
+            <h1 className='text-center text-2xl font-semibold pt-8'>Registration please!</h1>
+            <div className="hero pt-1">
+                <div className="hero-content">
+                    <div className='mr-6'>
+                        <Lottie animationData={lottie} loop={true} />
+                    </div>
+                    <div className="card  w-3/4  shadow-md bg-base-100">
+                        <form onSubmit={handleRegister} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="Name" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Confirm password</span>
+                                </label>
+                                <input type="password" name='confirm' placeholder="Confirm password" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">photo URL</span>
+                                </label>
+                                <input type="text" name='photo' placeholder="photo URL" className="input input-bordered" required />
+                            </div>
+                            <label className="label">
+                                <a href="#" className="label-text-alt ">Already have an account?
+                                    <Link className='link link-hover text-red-600 ml-2' to='/login'>Login</Link></a>
+                            </label>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Register</button>
+                            </div>
+                        </form>
+                        <p className='ml-8 mb-4' style={{ color: 'red' }}>{error}</p>
+                        <p className='ml-8 mb-4' style={{ color: 'green' }}>{success}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
