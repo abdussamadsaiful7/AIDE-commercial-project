3350a8599b0bc2961cea496d53e75e97fd15cc07
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-07-13 18:59:39 +0600

    added marquee

--- src/Components/HomePage/Trending/Trending.jsx
+++ src/Components/HomePage/Trending/Trending.jsx
@@ -2,19 +2,29 @@ import React, { useEffect, useState } from 'react';
 import ProductCard from './ProductCard';
 import { Link } from 'react-router-dom';
 import Marquee from "react-fast-marquee";
+import logo1 from '../../../assets/logo/logo1.png'
+import logo2 from '../../../assets/logo/logo2.png'
+import logo3 from '../../../assets/logo/logo3.png'
+import logo4 from '../../../assets/logo/logo4.png'
+import logo5 from '../../../assets/logo/logo5.png'
+import logo6 from '../../../assets/logo/logo6.png'
+import logo7 from '../../../assets/logo/logo7.png'
+import logo8 from '../../../assets/logo/logo8.png'
 
 const Trending = () => {
     const [products, setProducts] = useState([]);
     const [seeAll, setSeeAll] = useState(false);
-   
-    
-    const seeAllHandler = (stop) => {
+
+
+
+    const seeAllHandler = () => {
         setSeeAll(true);
-        
+        pauseOnClick(false)
     }
 
-    const seeLessHandler = (start) => {
+    const seeLessHandler = () => {
         setSeeAll(false);
+        pauseOnClick(true)
     }
 
     useEffect(() => {
@@ -23,29 +33,42 @@ const Trending = () => {
             .then(data => setProducts(data))
     }, [])
 
-    //onClick="document.getElementById('marquee1').stop()
-    //onClick="document.getElementById('marquee1').start()
+
 
     return (
-        <div className='pt-10 px-10'>
-
-            <div className='flex items-center justify-between'>
-                <h1 className='text-xl font-bold'>TRENDING NOW</h1>
-                {
-                    !seeAll && (<Link><a onClick={seeAllHandler}  >See all</a></Link>)
-                }
-                {
-                    seeAll && (<Link> <a onClick={seeLessHandler} >See less</a> </Link>)
-                }
-            </div>
+        <div>
             <Marquee>
+                <div className='grid grid-cols-8 gap-14 py-10'>
+                    <img className='w-32 h-32' src={logo1} alt="logo" />
+                    <img className='w-32 h-32' src={logo2} alt="logo" />
+                    <img className='w-32 h-32' src={logo3} alt="logo" />
+                    <img className='w-32 h-32' src={logo7} alt="logo" />
+                    <img className='w-32 h-32' src={logo4} alt="logo" />
+                    <img className='w-32 h-32' src={logo5} alt="logo" />
+                    <img className='w-32 h-32' src={logo6} alt="logo" />
+                    <img className='w-32 h-32' src={logo8} alt="logo" />
+                </div>
+            </Marquee>
+            <div className='pt-10 px-14'>
+
+                <div className='flex items-center justify-between'>
+                    <h1 className='text-xl font-bold'>TRENDING NOW</h1>
+                    {
+                        !seeAll && (<Link><a onClick={seeAllHandler}  >See all</a></Link>)
+                    }
+                    {
+                        seeAll && (<Link> <a onClick={seeLessHandler} >See less</a> </Link>)
+                    }
+                </div>
+
                 <div className='grid grid-cols-4 gap-4 pt-2'>
                     {
-                        products.slice(0, seeAll ? 10 : 4).map(product => <ProductCard 
+                        products.slice(0, seeAll ? 10 : 4).map(product => <ProductCard
                             product={product} key={product.name} />)
                     }
                 </div>
-            </Marquee>
+
+            </div>
         </div>
     );
 };
